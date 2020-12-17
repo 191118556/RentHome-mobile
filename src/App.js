ea207e6c39905bd9094866023f041c1982f53322
191118556 <191118556@qq.com>
2020-12-17 12:05:31 +0800

    路由初始配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import { Button } from 'antd-mobile';
+import { HashRouter, Route, Link } from 'react-router-dom'
+import Home from './pages/Home/index'
+import cityList from './pages/cityList/index'
 function App() {
   return (
     <div className="App">
-      首页
-      <Button type="primary">primary</Button>
+      <HashRouter>
+        <Link to='/'>首页</Link>
+        <Link to='/cityList'>城市</Link>
+
+        <Route exact path='/' component={Home} />
+        <Route exact path='/cityList' component={cityList} />
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/cityList/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h3>城市详情</h3>
+            </div>
+        )
+    }
+}
