fd4ee0de963440f5fd7341678c3442540c3f1635
191118556 <191118556@qq.com>
2020-12-25 00:29:44 +0800

    searchHeader头部搜索框的封装

--- src/pages/Rent/index.js
+++ src/pages/Rent/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { Flex } from 'antd-mobile';
 import Filter from './components/Filter'
+import { getCurrentCityInfo } from '../../utils'
+import styles from './index.module.scss';
+import SearchHeader from '../../components/SearchHeader'
 export default class Rent extends Component {
+    state = {
+        cityInfo: {}
+    }
+    async componentDidMount() {
+        const cityInfo = await getCurrentCityInfo();
+        this.setState({
+            cityInfo: cityInfo
+        })
+    }
     render() {
         return (
             <div>
+                <Flex className={styles.heander}>
+                    <i className="iconfont icon-back" onClick={() => this.props.history.go(-1)}></i>
+                    <SearchHeader cityName={this.state.cityInfo.label} className={styles.searchHeader}></SearchHeader>
+                </Flex>
                 <Filter />
             </div>
         )
